8c9f5a48406681b3936396ebebb4cea337dbc9f8
Brandon Abela <brandonabela9@gmail.com>
2021-07-06 00:32:33 +0200

    Add Initial Simulation GUI

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,5 +1,17 @@
+import GUI.SimulationWindow;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+
 public class GameOfLife {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+
+        SimulationWindow simulationWindow = new SimulationWindow();
+        simulationWindow.createWindow();
     }
 }
